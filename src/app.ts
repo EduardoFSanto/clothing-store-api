e5f9c9949350a1d0635bf72221330b79e9861fc8
Eduardo Farias <farias.eduardo@icloud.com>
2026-09-23 22:05:52 -0300

    feat: register new application modules

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,14 @@ import express from "express";
 
 import { errorHandler } from "./middleware/error-handler.js";
 
+import { addressesRoutes } from "./modules/addresses/addresses.routes.js";
 import { categoriesRoutes } from "./modules/categories/categories.routes.js";
-
 import { customersRoutes } from "./modules/customers/customers.routes.js";
-
 import { ordersRoutes } from "./modules/orders/orders.routes.js";
-
 import { paymentsRoutes } from "./modules/payments/payments.routes.js";
-
 import { productVariantsRoutes } from "./modules/product-variants/product-variants.routes.js";
-
 import { productsRoutes } from "./modules/products/products.routes.js";
-
+import { shippingRoutes } from "./modules/shipping/shipping.routes.js";
 import { stockRoutes } from "./modules/stock/stock.routes.js";
 
 export const app = express();
@@ -44,6 +40,16 @@ app.use(
 app.use(
   "/customers",
   customersRoutes,
+);
+
+app.use(
+  "/addresses",
+  addressesRoutes,
+);
+
+app.use(
+  "/shipping",
+  shippingRoutes,
 );
 
 app.use(
